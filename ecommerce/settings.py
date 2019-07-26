ffdac0fdce03c7fd312ad530d900773d43374967
timochfati <lamrini788@gmail.com>
2019-07-26 21:32:46 +0000

    added stripe

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     )
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
